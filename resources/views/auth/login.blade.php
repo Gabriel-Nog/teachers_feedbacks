36f8f7018df48c0533a37d85a3baa9ce680f7620
Gabriel-Nog <nogueirajoao799@gmail.com>
2024-05-02 17:51:11 -0300

    Pasta de icones e tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,33 @@
-<div class="container mx-auto">
-        <h1>Entre na sua conta</h1>
-        <form action="POST">
-            <div class="grid grid-cols-1 divide-y-130">
-                <div class="mb-82"><input type="email" name="email" id="email"></div>
-                <div><input type="password" name="password" id="password"></div>
-                <div><input type="submit" value="Entrar"></div>
-            </div>  
-        </form>
-</div>
+@extends('welcome')
+
+@section('title', 'Login')
+
+@section('content')
+    <!--  Application Details Start -->
+    <div class="w-full bg-grey-500">
+        <div class="container mx-auto py-8">
+            <div class="w-96 mx-auto bg-white rounded shadow">
+
+                <div class="mx-16 py-4 px-8 text-black text-xl font-bold border-b border-grey-500">Entre na sua conta
+                </div>
+
+                <form action="POST">
+                    <div class="py-4 px-8">
+                        <div class="mb-4">
+                            <input class=" border rounded border-[#339CFF] w-full py-2 px-3 text-grey-darker" type="text"
+                                name="email_login" id="email_login" placeholder="usuario@email.com">   
+                        </div>
+                        <div class="mb-4">
+                            <input class=" border rounded border-[#339CFF] w-full py-2 px-3 text-grey-darker" type="password"
+                            name="password_login" id="password_login" placeholder="Senha do usuário">                             
+                        </div>
+                        <div class="mb-4">
+                            <input type="submit"
+                                class="mb-2 mx-10 py-1 px-24 bg-[#0B3B60] text-[#fff] font-[700]" value="Enviar"
+                            >
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
