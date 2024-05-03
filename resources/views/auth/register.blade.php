a2ae6bea3c6caa2d729211ba321ac9632212681c
KpSantiago <santiago.kauajose11@gmail.com>
2024-05-03 17:23:04 -0300

    feat: removing forget password link

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,7 +45,7 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-         
+
         <!-- Role -->
         {{-- <div class="col-start-1 col-end-3">
             <x-input-label for="roles" :value="__('Atribuição')" />
@@ -61,11 +61,6 @@
         </div> --}}
 
         <div class="flex items-center justify-end col-span-2">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
