6a530140976a17ead9ddcaeb2ceed8dc73734a34
KpSantiago <santiago.kauajose11@gmail.com>
2024-05-03 16:15:26 -0300

    feat: add styles to subjects form

--- resources/views/subjects/subjectsRegister.blade.php
+++ resources/views/subjects/subjectsRegister.blade.php
@@ -1,17 +1,15 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('subjects.subjectsRegister.store') }}" class="grid grid-cols-2 gap-4">
+    <form method="POST" action="{{ route('subjects.subjectsRegister.store') }}" class="flex flex-col gap-3">
         @csrf
 
         <!-- Name -->
-        <div class="col-auto col-end-1">
-            <x-input-label for="name" :value="__('Disciplina para adicionar:')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-                autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                <x-primary-button class="ms-2 mt-5 col-auto">
-                    {{ __('Registrar') }}
-                </x-primary-button>
-        </div>
+        <x-input-label for="name" :value="__('Disciplina para adicionar:')" />
+        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+            autofocus autocomplete="name" />
+        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <x-primary-button class="mt-2 w-fit">
+            {{ __('Registrar') }}
+        </x-primary-button>
 
         </div>
     </form>
