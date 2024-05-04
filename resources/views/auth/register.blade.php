7703c3abf013bdb0f5aa096a7f0683838d00ef76
KpSantiago <santiago.kauajose11@gmail.com>
2024-05-04 00:26:11 -0300

    fix: fiex role_id syntax error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -49,15 +49,15 @@
         <!-- Role -->
         <div class="col-start-1 col-end-3">
             <x-input-label for="roles" :value="__('Atribuição')" />
-            <x-input-error :messages="$errors->get('roles')" class="mt-2" />
-                <select {{ $errors->has('roles_id') ? 'is-invalid' : '' }}
-                    class="max-w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1"
-                    name="roles_id" id="Roles">
-                    <option disabled select>Selecione a atribuição</option>
-                    @foreach ($roles as $role)
-                        <option value="{{ $role->id }}">{{ $role->name }}</option>
-                    @endforeach
-                </select>
+            <select {{ $errors->has('role_id') ? 'is-invalid' : '' }}
+                class="max-w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1"
+                name="role_id" id="roles">
+                <option disabled select>Selecione a atribuição</option>
+                @foreach ($roles as $role)
+                    <option value="{{ $role->id }}">{{ $role->name }}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('role_id')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end col-span-2">
