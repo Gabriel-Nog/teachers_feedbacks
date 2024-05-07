b8cefce9462bb5863f09e06115acd19bdbed1b89
KpSantiago <santiago.kauajose11@gmail.com>
2024-05-07 09:41:23 -0300

    feat: change admin account data configuration

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -50,9 +50,9 @@ class PermissionsSeeder extends Seeder
         $student->givePermissionTo(['comment']);
 
         $admin = User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => Hash::make(env('ADMIN_PASSWORD', 'admin$123')),
+            'name' => 'Usuário Administrador',
+            'email' => 'admin@email.com',
+            'password' => Hash::make(env('ADMIN_PASSWORD', '12345678')),
             'cpf' => '12345678912',
             'role_id' => '1'
         ]);
