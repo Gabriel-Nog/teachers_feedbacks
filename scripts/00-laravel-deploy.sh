029b746676b5ae41455c0e20b0a457cceacf7c3f
Gabriel-Nog <nogueirajoao799@gmail.com>
2024-05-08 08:43:42 -0300

    v.1.3

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -16,12 +16,24 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
+echo "Ruuning Seeders..."
+php artisan db:seed PermissionsSeeder
 
 echo "Running npm depedencies..."
-npm install -g require-global
+echo "installs NVM (Node Version Manager)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-# echo "Running static elemenrs..."
-# npm run dev
+echo "download and install Node.js"
+nvm install 20
+
+echo "verifies the right Node.js version is in the environment"
+node -v # should print `v20.13.0`
+
+echo "verifies the right NPM version is in the environment"
+npm -v # should print `10.5.2`
+
+echo "Running static elemenrs..."
+npm run dev
 
 
 
