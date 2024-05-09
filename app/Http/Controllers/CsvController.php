110967b24369a6b01351b6a1c5a0dc5b95c2681e
KpSantiago <santiago.kauajose11@gmail.com>
2024-05-09 16:52:07 -0300

    feat: create a resgister by csv file

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CsvController extends Controller
@@ -11,7 +12,65 @@ class CsvController extends Controller
         return view('csv.add-csv');
     }
 
-    public function createUserByCsvFile(Request $request){
+    public function importUserCsv(Request $request)
+    {
+        // $request->validate([
+        //     'csv-input' => 'required|mimes:csv',
+        // ]);
+        //read csv file and skip data
+        $file = $request->file('csvInput');
+        $handle = fopen($file->path(), 'r');
 
+        //skip the header row
+        fgetcsv($handle);
+
+
+        $chunkSize = 200;
+        while (!feof($handle)) {
+            $chunkData = [];
+
+            for ($i = 0; $i < $chunkSize; $i++) {
+                $data = fgetcsv($handle);
+
+                if ($data == false) {
+                    break;
+                }
+
+                $chunkData[] = $data;
+            }
+
+            $this->createUser($chunkData);
+        }
+
+        fclose($handle);
+    }
+    public function createUser($chunkData)
+    {
+
+
+        foreach ($chunkData as $line) {
+            $user = $line;
+
+            $role = 0;
+            switch ($user[3]) {
+                case 'Aluno':
+                    $role = 3;
+                    break;
+                case 'Professor':
+                    $role = 2;
+                    break;
+                default:
+                    $role = 3;
+                    break;
+            }
+
+            User::create([
+                "name" => $user[0],
+                "email" => $user[1],
+                "cpf" => $user[2],
+                'password' => $user[3],
+                'role_id' => $role
+            ]);
+        }
     }
 }
